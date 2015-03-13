e8a72a43fef2a6487c2bc23e87e44723de9c60db
kejun.gao <kejun.gao@amlogic.com>
2015-03-13 14:30:01 +0800

    wq example 5

--- hello/wq_exam.c
+++ hello/wq_exam.c
@@ -13,50 +13,31 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 //static void linkwatch_event(struct work_struct *dummy);
 
+static struct delayed_work mywork;
 static void work_cb(struct work_struct *);
 
-static DECLARE_WORK(mywork, work_cb);
 
-
-static struct workqueue_struct *my_workqueue;
-
-struct timer_list my_timer;
-
-void test_timer_fn(unsigned long arg)
-{
-    my_timer.expires += 3 * HZ;
-
-    add_timer(&my_timer);
-    queue_work(my_workqueue, &mywork);
-}
 void work_cb(struct work_struct *arg)
 {
     printk("====EAT EAT====\n");
-    dump_stack();
+	schedule_delayed_work(&mywork, 3 * HZ);
 }
 
 int wq_init(void)
 {
-    init_timer(&my_timer);
-    my_timer.function = test_timer_fn;
-    my_timer.expires = jiffies + 3 * HZ;
-    add_timer(&my_timer);
+    INIT_DELAYED_WORK(&mywork, work_cb);
+	schedule_delayed_work(&mywork, 3 * HZ);
 
-    my_workqueue = 
-        //create_singlethread_workqueue("test-wq");
-        //alloc_workqueue("test-wq", WQ_UNBOUND | WQ_MEM_RECLAIM, 1);
-        __alloc_workqueue_key("test-wq", WQ_UNBOUND | WQ_MEM_RECLAIM, 1, NULL, NULL);
     printk("test-wq init success\n");
 
     return 0;
 }
 void wq_exit(void)
 {
-    del_timer(&my_timer);
-
-    destroy_workqueue(my_workqueue);
+	cancel_delayed_work(&mywork);
     printk("wq exit success\n");
 }
+
 module_init(wq_init);
 module_exit(wq_exit);
 
